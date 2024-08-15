b6fd21b80d03f9a587f74d22d669531cb0af5630
whitgroves <whitney.groves@gmail.com>
2024-08-15 15:25:33 -0500

    encounters - routes 13, 14, 15

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -1,29 +1,15 @@
 Route13WildMons:
 	def_grass_wildmons 20 ; encounter rate
-IF DEF(_RED)
-	db 24, ODDISH
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
-	db 25, DITTO
-	db 28, GLOOM
-	db 30, GLOOM
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 25, DITTO
-	db 28, WEEPINBELL
-	db 30, WEEPINBELL
-ENDC
+	db 19, BELLSPROUT
+	db 19, ODDISH
+	db 25, VENONAT
+	db 24, WEEPINBELL
+	db 24, GLOOM
+	db 26, MANKEY
+	db 25, FARFETCHD
+	db 21, EKANS
+	db 27, MANKEY
+	db 30, ARBOK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,27 +1,15 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
-IF DEF(_RED)
-	db 24, ODDISH
-	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, ODDISH
-	db 28, VENONAT
-	db 26, ODDISH
-	db 30, GLOOM
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, BELLSPROUT
-	db 28, VENONAT
-	db 26, BELLSPROUT
-	db 30, WEEPINBELL
-ENDC
-	db 28, PIDGEOTTO
-	db 30, PIDGEOTTO
+	db 20, BELLSPROUT
+	db 20, ODDISH
+	db 27, VENONAT
+	db 24, WEEPINBELL
+	db 24, GLOOM
+	db 27, MANKEY
+	db 28, PERSIAN
+	db 29, TANGELA
+	db 28, PRIMEAPE
+	db 31, VENOMOTH
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
