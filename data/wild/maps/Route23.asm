11a330e833217f1621081c67d1ec0ebc89c4bdf6
whitgroves <whitney.groves@gmail.com>
2024-08-15 19:22:48 -0500

    encounters - route 23

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,25 +1,15 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
-	db 26, EKANS
-ENDC
-IF DEF(_BLUE)
-	db 26, SANDSHREW
-ENDC
-	db 33, DITTO
-	db 26, SPEAROW
-	db 38, FEAROW
-	db 38, DITTO
-	db 38, FEAROW
-IF DEF(_RED)
-	db 41, ARBOK
-ENDC
-IF DEF(_BLUE)
-	db 41, SANDSLASH
-ENDC
-	db 43, DITTO
-	db 41, FEAROW
-	db 43, FEAROW
+	db 37, PRIMEAPE
+	db 36, ARBOK
+	db 35, FEAROW
+	db 39, RAPIDASH
+	db 39, DODRIO
+	db 38, PIDGEOT
+	db 40, PINSIR
+	db 42, SCYTHER
+	db 41, TAUROS
+	db 38, KANGASKHAN
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
